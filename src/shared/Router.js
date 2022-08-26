11337250f1dff805f867a9a6dcd248c49a719260
whitebsm93 <whitebsm93@naver.com>
2022-08-26 18:47:03 +0900

    package 설치, rauter.js 작성
    - react-router-dom
    - react-redux
    - redux-toolkit
    - styled components

---
+++ src/shared/Router.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Write from "../pages/Write";
+import Detail from "../pages/Detail";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="write" element={<Write />} />
+        <Route path="detail/:id" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
