f4e7aa620474b106d18a48c7d3cc2e0e9230244d
whitebsm93 <whitebsm93@naver.com>
2022-08-26 18:49:55 +0900

    Update App.js
    - Router 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Router from "./shared/Router";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return <Router />;
+};
 
 export default App;
