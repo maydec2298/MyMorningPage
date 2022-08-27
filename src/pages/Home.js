4208a93d60e3befc2042d1e0e711828c4a47a701
whitebsm93 <whitebsm93@naver.com>
2022-08-27 16:45:34 +0900

    Squashed commit of the following:
    commit 2fb2f3aa5e5851d6383f0108b9169c8ce7fddacb
    Author: whitebsm93 <whitebsm93@naver.com>
    Date:   Sat Aug 27 16:42:59 2022 +0900
    
        모듈 테스트 완료
    
        action별로 테스트 완료
    
    commit 98fd63354bfba6955e76086efb75e4ecc7ca9abe
    Author: whitebsm93 <whitebsm93@naver.com>
    Date:   Sat Aug 27 16:20:17 2022 +0900
    
        module 1차 작업 완료
    
        postsSlice 완료
        - action:add, delete, editToggle
        commentsSlice 완료
        - action: acc, delete, editToggle
        store에 모듈 등록 완료
    
    commit f32a14dd4d090a3a299dcf047d03f9c22b5a593f
    Author: whitebsm93 <whitebsm93@naver.com>
    Date:   Sat Aug 27 04:24:45 2022 +0900
    
        Container component 완료
    
        - margin: 10px;
        - padding: 10px;
        추후 조정 가능함
         - border-radius: 12px;
         - box-shadow: 0 1px 8px #1f6aad;
        테두리 컬러나 둥근모양은 논의 필요함

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,39 @@
 import React from "react";
+import Container from "../components/UI/Container";
+import { useDispatch, useSelector } from "react-redux";
+import { addPost, deletePost } from "../redux/modules/postsSlice";
 
 const Home = () => {
-  return <h1>폰트적용 잘됐나?</h1>;
+  const posts = useSelector((state) => state.posts);
+  const comments = useSelector((state) => state.comments);
+
+  const dispatch = useDispatch();
+
+  const onAddHandler = () => {
+    dispatch(
+      addPost({
+        postId: 4,
+        userId: "추가확인",
+        title: "추가되는지...",
+        content: "확인중",
+        editToggle: false,
+      })
+    );
+  };
+
+  const onDeleteHandler = () => {
+    dispatch(deletePost(2));
+  };
+
+  console.log(posts);
+
+  return (
+    <Container>
+      <h1>폰트적용 잘됐나?</h1>
+      <button onClick={onAddHandler}>추가?</button>
+      <button onClick={onDeleteHandler}>2번글을 없애볼게</button>
+    </Container>
+  );
 };
 
 export default Home;
