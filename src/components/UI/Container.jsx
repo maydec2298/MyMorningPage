84206692437b594c2cb11aaa3c105fc01bffe5e4
whitebsm93 <whitebsm93@naver.com>
2022-08-30 09:44:20 +0900

    Update Container.jsx
    - margin 값 변경
    display flex 제거

--- src/components/UI/Container.jsx
+++ src/components/UI/Container.jsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-  margin: 10px;
+  margin: 10px 3px;
   padding: 10px;
   box-shadow: 0 1px 8px #1f6aad;
-  display: flex;
+  /* display: flex; */
 `;
 
 export default Container;
