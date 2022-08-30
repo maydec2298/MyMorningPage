cc872d30230963e647e7780fb8ca03916ad47ac5
whitebsm93 <whitebsm93@naver.com>
2022-08-31 02:17:34 +0900

    Home.js 수정 및 자잘한 수정
    <Home.js>
    - db test용 코드 및 import 삭제
    - 글 목록 타이틀 추가
    <그 외>
    - PostDetail, useInput에서 필요없는 Import 삭제

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,5 @@
-import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { __getPosts } from "../redux/modules/postsSlice";
+import styled from "styled-components";
 // import Components
 import Layout from "../components/UI/Layout";
 import Container from "../components/UI/Container";
@@ -10,37 +8,20 @@ import PostList from "../components/Posts/PostList";
 
 const Home = () => {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const { isLoading, error, posts } = useSelector((state) => state.posts);
-
-  useEffect(() => {
-    dispatch(__getPosts());
-  }, [dispatch]);
-
-  if (isLoading) {
-    return <div>로딩 중....</div>;
-  }
-
-  if (error) {
-    return <div>{error.message}</div>;
-  }
-
   return (
     <Layout title="My Morning Page">
       <Container>
-        <Button
-          add
-          onClick={() => {
-            navigate("/write");
-          }}
-        >
-          글쓰기
-        </Button>
-        <div>
-          {/* {posts.map((post) => (
-            <div key={post.id}>{post.title}</div>
-          ))} */}
-        </div>
+        <TitleBox>
+          <TitleH2>글 목록</TitleH2>
+          <Button
+            add
+            onClick={() => {
+              navigate("/write");
+            }}
+          >
+            글쓰기
+          </Button>
+        </TitleBox>
         <PostList />
       </Container>
     </Layout>
@@ -48,3 +29,15 @@ const Home = () => {
 };
 
 export default Home;
+
+const TitleBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 5px;
+`;
+
+const TitleH2 = styled.h2`
+  margin: 0;
+`;
